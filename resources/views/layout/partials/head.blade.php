d35302aa53dbb3999b14fa4ce36a844fc6f0fbed
BVNKFATBOY <theerapong.bvnk@gmail.com>
2022-09-06 20:28:57 +0700

    Initial commit

---
+++ resources/views/layout/partials/head.blade.php
@@ -0,0 +1,26 @@
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="author" content="">
+<title>Student</title>
+
+<!-- css  -->
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" crossorigin="anonymous">
+<link rel="stylesheet" href="/css/style.css">
+
+<style>
+      .bd-placeholder-img {
+    font-size: 1.125rem;
+    text-anchor: middle;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    user-select: none;
+  }
+  @media (min-width: 768px) {
+    .bd-placeholder-img-lg {
+      font-size: 3.5rem;
+    }
+  }
+</style>
